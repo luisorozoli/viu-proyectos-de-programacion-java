6dc618c93f926bef8e5ef91946e65110d88ffb89
luisorozoli <lorozco@matchbyte.com>
2020-11-23 12:15:04 +0100

    Cambio de interfaz de consola a AWT Grafica (Menu))

--- Supercomputacion/src/supercomputacion.java
+++ Supercomputacion/src/supercomputacion.java
@@ -8,62 +8,126 @@
  *
  * @author Sebastian Plaza, Gonzalo Diaz, Luis Orozco.
  */
-import java.util.InputMismatchException;
-import java.util.Scanner;
- 
-public class supercomputacion {
- 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
- 
-        Scanner sn = new Scanner(System.in);
-        boolean salir = false;
-        int opcion; //Guardaremos la opcion del usuario
- 
-        while (!salir) {
- 
-            System.out.println("1. Usuarios");
-            System.out.println("2. Centros");
-            System.out.println("3. Trabajos");
-            System.out.println("4. Asignar Trabajos");
-            System.out.println("5. Información");
-            System.out.println("6. Salir");
- 
-            try {
- 
-                System.out.println("Escribe una de las opciones");
-                opcion = sn.nextInt();
- 
-                switch (opcion) {
-                    case 1:
-                        System.out.println("Has seleccionado la opcion 1 - USUARIOS");
-                        break;
-                    case 2:
-                        System.out.println("Has seleccionado la opcion 2 - CENTROS");
-                        break;
-                    case 3:
-                        System.out.println("Has seleccionado la opcion 3 - TRABAJOS");
-                        break;
-                    case 4:
-                        System.out.println("Has seleccionado la opcion 4 - ASIGNAR TRABAJOS");
-                        break;
-                    case 5:
-                        System.out.println("Has seleccionado la opcion 5 - INFORMACIÓN");
-                        break;
-                    case 6:
-                        salir = true;
-                        break;
-                    default:
-                        System.out.println("Solo números entre 1 y 6");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Debes insertar un número");
-                sn.next();
+import java.awt.*;
+import java.awt.event.*;
+
+public class supercomputacion{
+    private Frame ventana;
+
+    public supercomputacion(){
+        ventana = new Frame("Supercomputacion");
+        MenuBar principal = new MenuBar();
+
+        //crearemos un menú clásico para que se entienda la idea
+        Menu usuarios = new Menu("Usuarios");
+            MenuItem usuarios_accion1 = new MenuItem("Alta de usuario");
+            MenuItem usuarios_accion2 = new MenuItem("Baja de usuario");
+            MenuItem usuarios_accion3 = new MenuItem("Modificación de usuario");
+            MenuItem usuarios_salir = new MenuItem("Salir");
+        
+        Menu centros = new Menu("Centros");
+            MenuItem centros_accion1 = new MenuItem("Alta de centro");
+            MenuItem centros_accion2 = new MenuItem("Baja de centro");
+            MenuItem centros_accion3 = new MenuItem("Modificaión de centro");
+        
+        Menu trabajos = new Menu("Trabajos");
+            MenuItem trabajos_accion1 = new MenuItem("Crear trabajo");
+            MenuItem trabajos_accion2 = new MenuItem("Modificar trabajo");
+            MenuItem trabajos_accion3 = new MenuItem("Eliminar trabajo");
+        
+        Menu informacion = new Menu("Informacion");
+            MenuItem informacion_accion1 = new MenuItem("Colas de trabajo de cada centro");
+            MenuItem informacion_accion2 = new MenuItem("Trabajos sin asignar");
+            MenuItem informacion_accion3 = new MenuItem("Trabajos en proceso en cada centro");
+            MenuItem informacion_accion4 = new MenuItem("Histórico de trabajos");
+        
+        Menu salir = new Menu("Salir");
+
+        //ahora, agregamos los elementos del menú USUARIOS.
+        usuarios.add(usuarios_accion1);
+        usuarios.add(usuarios_accion2);
+        usuarios.add(usuarios_accion3);
+        usuarios.addSeparator(); //agrega línea divisoria
+        usuarios.add(usuarios_salir);
+        
+        //ahora, agregamos los elementos del menú CENTROS.
+        centros.add(centros_accion1);
+        centros.add(centros_accion2);
+        centros.add(centros_accion3);
+        centros.addSeparator(); //agrega línea divisoria
+        
+        //ahora, agregamos los elementos del menú TRABAJOS.
+        trabajos.add(trabajos_accion1);
+        trabajos.add(trabajos_accion2);
+        trabajos.add(trabajos_accion3);
+        trabajos.addSeparator(); //agrega línea divisoria
+        
+        //ahora, agregamos los elementos del menú INFORMACION.
+        informacion.add(informacion_accion1);
+        informacion.add(informacion_accion2);
+        informacion.add(informacion_accion3);
+        informacion.add(informacion_accion4);
+        informacion.addSeparator(); //agrega línea divisoria
+                
+         //agregar el listener para los elementos del menú
+        EscuchaItemMenu eim = new EscuchaItemMenu();
+            usuarios_accion1.addActionListener(eim);
+            usuarios_accion2.addActionListener(eim);
+            usuarios_accion3.addActionListener(eim);
+            usuarios_salir.addActionListener(eim);
+            
+            centros_accion1.addActionListener(eim);
+            centros_accion2.addActionListener(eim);
+            centros_accion3.addActionListener(eim);
+            
+            trabajos_accion1.addActionListener(eim);
+            trabajos_accion2.addActionListener(eim);
+            trabajos_accion3.addActionListener(eim);
+            
+            informacion_accion1.addActionListener(eim);
+            informacion_accion2.addActionListener(eim);
+            informacion_accion3.addActionListener(eim);
+            informacion_accion4.addActionListener(eim);
+        
+        //ahora, indicamos que el menú "principal" forma parte de la barra de menú
+        principal.add(usuarios);
+        principal.add(centros);
+        principal.add(trabajos);
+        principal.add(informacion);
+
+        //Por último, le decimos a la ventana cuál es su barra de menu
+        ventana.setMenuBar(principal);
+
+        ventana.setSize(300, 300);
+        ventana.setResizable(false);
+        ventana.addWindowListener(new WindowListener(){
+            public void windowOpened(WindowEvent e){}
+            public void windowActivated(WindowEvent e){}
+            public void windowDeactivated(WindowEvent e){}
+            public void windowIconified(WindowEvent e){}
+            public void windowDeiconified(WindowEvent e){}
+            public void windowClosed(WindowEvent e){}
+            public void windowClosing(WindowEvent e){
+                ventana.setVisible(false);
+                ventana.dispose();
+            }
+        });
+        ventana.setVisible(true);
+    }
+
+    class EscuchaItemMenu implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            MenuItem item = (MenuItem)e.getSource();
+            String texto = item.getLabel();
+            System.out.println("Opción seleccionada: " + texto);
+            if (texto.equals("Salir")){
+                ventana.setVisible(false);
+                ventana.dispose();
             }
         }
- 
     }
- 
+
+    public static void main(String[] args){
+        supercomputacion pm = new supercomputacion();
+    }
 }
